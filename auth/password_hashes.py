b278eef422a4c9a6f61fc6f33450cd7c37caf4fd
Iann29 <iansingshau@gmail.com>
2025-04-12 07:37:40 -0300

    dados sensiveis, mas fds ta privado

---
+++ auth/password_hashes.py
@@ -0,0 +1,15 @@
+"""
+Arquivo contendo os hashes de senha para testes e desenvolvimento.
+NÃO ENVIAR ESTE ARQUIVO PARA O GITHUB.
+"""
+
+PASSWORD_HASHES = {
+    "abacaxi":  "613c7c5ce61f252eedba18078a6df9df83899f09936792f0d4942743c8edfbec",
+    "morango":  "ac8d90c8617180df03ff51db2d33e8a7f366102465c741c141f74ffc78d048a0",
+    "banana":   "6b4c955abc1b020f5fbed28663d8922add54c46134b00d61832f4e3e24065ab8",
+    "laranja":  "f65c1159a1129f0fc647744af8088341ae2693c94a148a6266f56040c1a43c02",
+    "uva":      "1c6b110363e6a55a2ab18b96fa52cabbada3ce78ae8049bafb972b9bacd329a8",
+    "manga":    "08502fccc154f4d52e43e6400731ff39ade195630491c6061c0d2ca0160bccb4",
+    "kiwi":     "76b5b480e658b58bfe8b030271e6264ad56da2915d4238585b76697159233fb7",
+    "melancia": "57a3e40df21636e0fd29fdf4c1bf66d6a87c5cdf0ad842a777f2315ef9cc6bd1"
+}
